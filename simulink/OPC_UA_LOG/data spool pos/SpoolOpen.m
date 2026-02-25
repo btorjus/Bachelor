395abf8caa3f240ee54656a6236641a2936acba3
Torjus B <torjusb@uia.no>
2026-02-25 09:23:11 +0100

    Start of measurements
    Did one run of tests, not sure if good data. Matlab mechanics and plot for cylinder force

--- simulink/OPC_UA_LOG/data spool pos/SpoolOpen.m
+++ simulink/OPC_UA_LOG/data spool pos/SpoolOpen.m
@@ -1,28 +1,26 @@
-clc; close all; 
+clc; close all;
 
 load torjus1.mat
 
+%% 1. Extract timetables
 PistonPos = data{1}.extractTimetable;
-SpoolPos = data{2}.extractTimetable;
+SpoolPos  = data{2}.extractTimetable;
 
-hfig = figure;  % save the figure handle in a variable
-hold on
-plot(PistonPos.Time, PistonPos.Variables,'color',[1 0.38 0.51],'LineWidth',1.5,'DisplayName','Ufiltrert');
-plot(SpoolPos.Time, SpoolPos.Variables,'color',[174 129 255]./255,'LineWidth',1.5,'DisplayName','$\tau = 0.2$');
-xlabel('tid $t$ (s)')
-ylabel('Signal')
-xlim([seconds(31.36) seconds(31.46)])
-fname = 'SpoolActive';
-legend('Location','northwest')
+%% 2. Select time interval (31.4s to 32.0s)
+t_start = seconds(31.38);
+t_end   = seconds(32.0);
 
-picturewidth = 20; % set this parameter and keep it forever
-hw_ratio = 0.65; % feel free to play with this ratio
-set(findall(hfig,'-property','FontSize'),'FontSize',17) % adjust fontsize to your document
+PistonPos_sel = PistonPos(PistonPos.Time >= t_start & PistonPos.Time <= t_end, :);
+SpoolPos_sel  = SpoolPos(SpoolPos.Time >= t_start & SpoolPos.Time <= t_end, :);
 
-set(findall(hfig,'-property','Box'),'Box','off') % optional
-set(findall(hfig,'-property','Interpreter'),'Interpreter','latex') 
-set(findall(hfig,'-property','TickLabelInterpreter'),'TickLabelInterpreter','latex')
-set(hfig,'Units','centimeters','Position',[3 3 picturewidth hw_ratio*picturewidth])
-pos = get(hfig,'Position');
-set(hfig,'PaperPositionMode','Auto','PaperUnits','centimeters','PaperSize',[pos(3), pos(4)])
-print(hfig,fname,'-dpdf','-painters','-fillpage')
+%% 3. Synchronize onto a common time vector
+combined = synchronize(PistonPos_sel, SpoolPos_sel, 'union', 'linear');
+
+%% 4. Convert to regular table with time in plain seconds
+T = timetable2table(combined);
+T.Time = seconds(T.Time);
+
+%% 5. Export to CSV with semicolon delimiter
+writetable(T, 'torjus1_export.csv', 'Delimiter', ';');
+
+fprintf('Done! Exported %d rows to torjus1_export.csv\n', height(T));
