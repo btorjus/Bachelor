707e77a636cf60af48e8651e5a59f814806823b5
Kim Robin Jensen <kimrje@uia.no>
2026-04-14 13:52:55 +0200

    Twincat Update
    Torjus er på ferie og jeg gitter i vei

--- data/SineWave/FFPlotting.m
+++ data/SineWave/FFPlotting.m
@@ -8,6 +8,10 @@ dataNoFF    = loadCSV('SineWave_NoFF_PF_KVFF_100bar_0.05freq_31.03.26.csv');
 dataNoPosFB = loadCSV('SineWave_NoPositionFeedback_PF_KVFF_100bar_0.05freq_31.03.26.csv');
 dataOtherFF = loadCSV('SineWave_OtherFF_PF_100bar_0.05freq_31.03.26.csv');
 dataOtherFF2 = loadCSV('SineWave_Other_FF_100bar_0.05freq_fUFF_08.04.26.csv');
+data8Grade = loadCSV('SineWave_8gradePoly_PF_KVFF_100bar_0.05freq_10.04.26.csv');
+data019 = loadCSV('SineWave_PF_KVFF_100bar_0.05freq_10.04.26.csv');
+%%
+
 
 
 
@@ -95,14 +99,67 @@ lg.Position(2) = lg.Position(2) - 0.019;
 
 saveFigure(hfig2, 'Subplot_KVFF')
 
-%% Kv FF contribution
-fU_Smooth = movmean(dataKVFF.fU, 1);
+% Kv FF contribution
+fU_Smooth = movmean(dataKVFF.fU, 1000);
 hfig4 = figure;
-plot(dataKVFF.fTimer, fU_Smooth, '-', 'Color', '#F92672', 'LineWidth', 1.5, 'DisplayName', '$u$')
+plot(dataKVFF.fTimer, fU_Smooth, '-', 'Color', '#000000', 'LineWidth', 2, 'DisplayName', '$u$')
 hold on
-plot(dataKVFF.fTimer, dataKVFF.fU_FF, '-', 'Color', '#1E90FF', 'LineWidth', 1.5, 'DisplayName', '$u_{FF}$')
-plot(dataKVFF.fTimer, dataKVFF.fUpfb, '-', 'Color', '#000000', 'LineWidth', 1.5, 'DisplayName', '$u_{PFB}$')
-plot(dataKVFF.fTimer, dataKVFF.fPID, '-', 'Color', '#2ECC71', 'LineWidth', 1.5, 'DisplayName', '$u_{PF}$')
+plot(dataKVFF.fTimer, dataKVFF.fU_FF, '-', 'Color', '#1E90FF', 'LineWidth', 2, 'DisplayName', '$u_{FF}$')
+%plot(dataKVFF.fTimer, dataKVFF.fUpfb, '-', 'Color', '#000000', 'LineWidth', 1.5, 'DisplayName', '$u_{PFB}$')
+%plot(dataKVFF.fTimer, dataKVFF.fPID, '-', 'Color', '#2ECC71', 'LineWidth', 1.5, 'DisplayName', '$u_{PF}$')
+hold off
+grid off
+xlim([8, 89])
+xlabel('Time [s]')
+ylabel('Signal [-]')
+title('Valve Signals, Active FF (100 bar, 0.05 Hz)')
+lg = legend('Interpreter', 'latex');
+%lg.Position(1) = lg.Position(1) - 0.020;
+%lg.Position(2) = lg.Position(2) - 0.019;
+saveFigure(hfig4, 'KvFF_Contribution')
+
+
+%% 8 Grade - Ikke i bruk
+% KVFF (Xdotref, Xdotactual, PistonPos.)
+dt = mean(diff(data8Grade.fTimer));
+vActualKVFF = gradient(data8Grade.fPistonPosition, dt);
+vSmoothKVFF = movmean(vActualKVFF, 1000);
+hfig2 = figure;
+subplot(2,1,1)
+plot(data8Grade.fTimer, data8Grade.fPistonPosition, 'k-', 'LineWidth', 1.5, 'DisplayName', 'Cylinder Position')
+hold on
+plot(data8Grade.fTimer, data8Grade.fXRef, '-', 'Color', '#F92672', 'LineWidth', 1.5, 'DisplayName', '$x_{ref}$')
+hold off
+grid off;
+xlim([7, 89]);
+%ylim([0.05, 0.35]);
+title('Active FF (100 bar, 0.05 Hz)')
+ylabel('Position [m]')
+lg = legend('Interpreter', 'latex');
+lg.Position(1) = lg.Position(1) + 0.07;
+lg.Position(2) = lg.Position(2) + 0.02;
+subplot(2,1,2)
+plot(data8Grade.fTimer, data8Grade.fXDotRef, '-', 'Color', '#F92672', 'LineWidth', 1.5, 'DisplayName', '$\dot{x}_{ref}$')
+hold on
+plot(data8Grade.fTimer, vSmoothKVFF, '-', 'Color', '#1E90FF', 'LineWidth', 1.5, 'DisplayName', '$\dot{x}_{actual}$')
+hold off
+grid off
+xlim([7, 89])
+%ylim([-0.06, 0.06])
+xlabel('Time [s]')
+ylabel('Velocity [m/s]')
+lg = legend('Interpreter', 'latex');
+lg.Position(1) = lg.Position(1) - 0.017;
+lg.Position(2) = lg.Position(2) - 0.019;
+saveFigure(hfig2, 'Subplot_KVFF')
+% Kv FF contribution
+fU_Smooth = movmean(data8Grade.fU, 1);
+hfig4 = figure;
+plot(data8Grade.fTimer, fU_Smooth, '-', 'Color', '#F92672', 'LineWidth', 1.5, 'DisplayName', '$u$')
+hold on
+plot(data8Grade.fTimer, data8Grade.fU_FF, '-', 'Color', '#1E90FF', 'LineWidth', 1.5, 'DisplayName', '$u_{FF}$')
+plot(data8Grade.fTimer, data8Grade.fUpfb, '-', 'Color', '#000000', 'LineWidth', 1.5, 'DisplayName', '$u_{PFB}$')
+plot(data8Grade.fTimer, data8Grade.fPID, '-', 'Color', '#2ECC71', 'LineWidth', 1.5, 'DisplayName', '$u_{PF}$')
 hold off
 grid on
 xlim([8, 89])
@@ -113,6 +170,9 @@ lg = legend('Interpreter', 'latex');
 %lg.Position(1) = lg.Position(1) - 0.020;
 %lg.Position(2) = lg.Position(2) - 0.019;
 saveFigure(hfig4, 'KvFF_Contribution')
+
+
+
 
 %%
 % Other FF
@@ -228,6 +288,43 @@ lg.Position(2) = lg.Position(2) - 0.03;
 
 saveFigure(hfig3, 'Subplot_NoPosFB_PosFB')
 
+%% PID Signal
+
+% NoPosFB vs KVFF - PistonPosition and XRef subplot (reordered)
+idxNoPosFB = dataNoPosFB.fTimer >= 0 & dataNoPosFB.fTimer <= 187;
+idxKVFF    = dataKVFF.fTimer    >= 0 & dataKVFF.fTimer    <= 85;
+hfig7 = figure;
+
+% --- Øverste subplot: tidligere nederste (KVFF PistonPosition + XRef) ---
+subplot(2,1,1)
+plot(dataKVFF.fTimer(idxKVFF), dataKVFF.fPistonPosition(idxKVFF), 'k-', 'LineWidth', 1.5, 'DisplayName', 'Cylinder Position')
+hold on
+plot(dataKVFF.fTimer(idxKVFF), dataKVFF.fXRef(idxKVFF), '-', 'Color', '#F92672', 'LineWidth', 1.5, 'DisplayName', '$x_{ref}$')
+hold off
+grid off;
+title('Active Position Feedback (100 bar, 0.05 Hz)')
+ylabel('Position [m]')
+xlim([8,85])
+ylim([0.04,0.5])
+lg = legend('location', 'northeast', 'Interpreter', 'latex');
+lg.Position(1) = lg.Position(1) + 0.07;
+lg.Position(2) = lg.Position(2) - 0.03;
+
+% --- Nederste subplot: fU og fPID mot tid ---
+subplot(2,1,2)
+plot(dataKVFF.fTimer(idxKVFF), dataKVFF.fU(idxKVFF), 'k-', 'LineWidth', 1.5, 'DisplayName', '$u$')
+hold on
+plot(dataKVFF.fTimer(idxKVFF), dataKVFF.fPID(idxKVFF), '-', 'Color', '#00C853', 'LineWidth', 1.5, 'DisplayName', '$u_{PID}$')
+hold off
+grid off;
+xlabel('Time [s]')
+ylabel('Control Signal [-]')
+xlim([8,85])
+lg = legend('location', 'northeast', 'Interpreter', 'latex');
+%lg.Position(1) = lg.Position(1) + 0.05;
+lg.Position(2) = lg.Position(2) - 0.03;
+
+saveFigure(hfig7, 'PID_Signal')
 
 
 
